abb11c354d343f472e879f7011fa072fb71cf826
RITHICK R <109365522+Rithick-555@users.noreply.github.com>
2024-06-17 22:29:25 +0530

    Add files via upload

---
+++ Header.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse nihil voluptate ullam, dicta quis accusantium atque quos illum animi repellendus molestias tempora, ab odit. Nulla, aliquid. Provident veniam quidem aliquid.</p>
+
+</head>
+<body>
+<?php
+// This is the header file
+// Add any other headers you need
+
+echo "<h1>This is the header file Helooo</h1>";
+?>
+    
+</body>
+</html>
